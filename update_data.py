a7dc1386382bab702086bc9c9d5dae37b2312c0d
Shun <s.tsuchiya.business@gmail.com>
2023-12-10 21:29:23 +0900

    :recycle: [refactor code] =>  formatting

--- update_data.py
+++ update_data.py
@@ -1,11 +1,18 @@
 from src.downloader.hatena import update_hatena_articles
 from src.downloader.zenn import update_zenn_articles
+from src.service.article_handler import write_articles_to_csv
 from src.models.site import WEB_SITE_LIST
 from src.service.supply_url import SiteSupply
+from src.models.article import Article
+from typing import List
 
 
 def create_site_list() -> SiteSupply:
     return SiteSupply(WEB_SITE_LIST)
+
+
+def add_new_articles_to_csv(new_articles: List[Article], csv_path: str) -> None:
+    write_articles_to_csv(new_articles, csv_path)
 
 
 if __name__ == "__main__":
@@ -14,6 +21,5 @@ if __name__ == "__main__":
         url=sites.get_url_by_name(name="zenn"), csv_path="src/techblog/data/zenn.csv"
     )
     update_hatena_articles(
-        url=sites.get_url_by_name(name="hatena"),
         csv_path="src/techblog/data/hatena.csv",
     )
